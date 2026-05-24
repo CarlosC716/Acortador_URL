44dfd7f9ace53e35eb13ee71fd11283a198a0512
CarlosC716 <kikecorrea109@gmail.com>
2026-05-23 21:45:14 -0500

    Implement URL repository and service methods

--- src/main/java/com/backend/acortador/repository/IUrlRepository.java
+++ src/main/java/com/backend/acortador/repository/IUrlRepository.java
@@ -1,6 +1,9 @@
 package com.backend.acortador.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.backend.acortador.entity.Url;
@@ -8,4 +11,13 @@ import com.backend.acortador.entity.Url;
 @Repository
 public interface IUrlRepository extends JpaRepository<Url, Integer> {
 
+	@Modifying(clearAutomatically = true)
+	@Query("""
+			UPDATE Url u
+			SET u.accessCount = u.accessCount + 1
+			WHERE u.shortCode = :code
+			""")
+	void incrementAccessCount(@Param("code") String code);
+
+	Url findUrlByShortCode(@Param("code") String code);
 }
--- src/main/java/com/backend/acortador/service/UrlService.java
+++ src/main/java/com/backend/acortador/service/UrlService.java
@@ -1,5 +1,78 @@
 package com.backend.acortador.service;
 
-public class UrlService {
+import java.time.LocalDateTime;
+import java.util.UUID;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.backend.acortador.dto.UrlRequest;
+import com.backend.acortador.dto.UrlResponse;
+import com.backend.acortador.entity.Url;
+import com.backend.acortador.mapper.UrlMapper;
+import com.backend.acortador.repository.IUrlRepository;
+
+import jakarta.transaction.Transactional;
+
+@Service
+public class UrlService {
+	@Autowired
+	private IUrlRepository _repo;
+	@Autowired
+	private UrlMapper _mapper;
+	
+	@Transactional
+	public UrlResponse findUrl(String code) {
+		_repo.incrementAccessCount(code);
+		Url u = _repo.findUrlByShortCode(code);
+		if (u == null) {
+			return null;
+		}
+
+		return _mapper.toResponse(u);
+	}
+
+	public UrlResponse getStats(String code) {
+		Url u = _repo.findUrlByShortCode(code);
+		return _mapper.toResponse(u);
+
+	};
+
+	public UrlResponse create(String url) {
+		Url u = new Url();
+
+		String code = UUID.randomUUID().toString().substring(0, 6);
+
+		u.setUrl(url);
+		u.setShortCode(code);
+		u.setCreatedAt(LocalDateTime.now());
+		u.setExpirationDate(LocalDateTime.now().plusDays(10));
+		Url saved = _repo.save(u);
+
+		return _mapper.toResponse(saved);
+	}
+
+	public UrlResponse update(UrlRequest url) {
+		Url u = _repo.findUrlByShortCode(url.getShortCode());
+
+		if (u == null) {
+			return null;
+		}
+
+		u.setUrl(url.getUrl()); 
+		u.setUpdatedAt(LocalDateTime.now());
+		u.setExpirationDate(LocalDateTime.now().plusDays(30));
+		Url saved = _repo.save(u);
+
+		return _mapper.toResponse(saved);
+	}
+
+	public Boolean delete(String url) {
+		Url u = _repo.findUrlByShortCode(url);
+		if (u != null) {
+			_repo.delete(u);
+			return true;
+		}
+		return false;
+	}
 }
